e71f6cedd4495eb8a31e9184be890972b5616710
ChaudhariJayant <153393882+ChaudhariJayant@users.noreply.github.com>
2025-11-21 23:35:42 -0500

    CNN_venv created

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
 __version__ = "0.0.0"
 
 REPO_NAME = "renal-tumor-detection"
 AUTHOR_USER_NAME = "ChaudhariJayant"
-SRC_REPO = "ccnClassifer "
+SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "chadmama1225@gmail.com"
 
 
@@ -17,7 +14,7 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for CCN app",
-    long_description=long_description,
+    # long_description=long_description,
     # long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
